861f10d902d253a56fd105f22a600bfe41a24f68
MohammadReza Pakzadian <pakzadianmrp@gmail.com>
2019-07-02 18:19:20 +0430

    add RSSRead method in DBOperation

--- src/main/java/nimbo/in/DBOperations.java
+++ src/main/java/nimbo/in/DBOperations.java
@@ -1,80 +1,66 @@
 package nimbo.in;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+
+import java.net.URL;
+import java.sql.*;
 
 public class DBOperations {
     // JDBC driver name and database URL
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost/";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/";
     private static final String DB_NAME = "RSSFeed";
     //  Database credentials
     private static final String USER = "root";
     private static final String PASS = "";
 
+    private static Connection conn = null;
+    private static Statement stmt = null;
+    private static int tableId;
 
     public static void createDB() {
-        Connection conn = null;
-        Statement stmt = null;
         try {
             Class.forName(JDBC_DRIVER);
 
             //STEP 3: Open a connection
             System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            conn = DriverManager.getConnection(DB_URL + "?useSsl=false&useUnicode=yes&characterEncoding=UTF-8", USER, PASS);
 
             //STEP 4: Execute a query
             System.out.println("Creating database...");
             stmt = conn.createStatement();
 
-            String sql = "CREATE DATABASE " + DB_NAME;
+            String sql = "CREATE DATABASE IF NOT EXISTS " + DB_NAME;
             stmt.executeUpdate(sql);
             System.out.println("Database created successfully...");
         } catch (SQLException ignored) {
             System.out.println("Database was exist!");
         } catch (Exception se) {
             se.printStackTrace();
-        } finally {
-            try {
-                if (stmt != null)
-                    stmt.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
         }
     }
 
     public static void createTables() {
-        Connection conn = null;
-        Statement stmt = null;
         try {
-            Class.forName(JDBC_DRIVER);
-
-            System.out.println("Connecting to a selected database...");
-            conn = DriverManager.getConnection(DB_URL + DB_NAME, USER, PASS);
-            System.out.println("Connected database successfully...");
+            conn = DriverManager.getConnection(DB_URL + DB_NAME + "?useSsl=false&useUnicode=yes&characterEncoding=UTF-8", USER, PASS);
+            stmt = conn.createStatement();
 
             System.out.println("Creating table in given database...");
-            stmt = conn.createStatement();
 
             System.out.println("creating RSSChannel table...");
 
             try {
-                String sql = "CREATE TABLE RSSChannel " +
-                        "(RSSLink VARCHAR(100) not NULL, " +
-                        " Title VARCHAR(100), " +
-                        " Link VARCHAR(100), " +
-                        " Description LONGTEXT, " +
-                        " LastBuildDate DATETIME, " +
-                        " PRIMARY KEY (RSSLink))";
+                String sql = "CREATE TABLE IF NOT EXISTS RSSChannel " +
+                        "(\nid int(11) NOT NULL AUTO_INCREMENT, " +
+                        " RSSLink TEXT not NULL, " +
+                        " Title TEXT, " +
+                        " Link TEXT, " +
+                        " Description TEXT, " +
+                        " LastBuildDate DATE, " +
+                        " PRIMARY KEY (id)) ENGINE=InnoDB CHARSET=utf8mb4;";
                 stmt.executeUpdate(sql);
 
                 stmt.executeUpdate("ALTER TABLE `RSSChannel` ADD INDEX(`RSSLink`)");
@@ -86,24 +72,73 @@ public class DBOperations {
 
             System.out.println("creating News table...");
             try {
-                String sql = "CREATE TABLE News\n" +
-                        "(" +
-                        "    id            INTEGER not NULL auto_increment,\n" +
-                        "    Title         VARCHAR(100),\n" +
-                        "    Link          VARCHAR(100),\n" +
-                        "    Author        VARCHAR(100),\n" +
-                        "    PublishedDate DATETIME,\n" +
-                        "    RSSLink     VARCHAR(100)     not null,\n" +
-                        "    FOREIGN KEY (RSSLink) REFERENCES RSSChannel (RSSLink) ON UPDATE CASCADE ON DELETE RESTRICT,\n" +
-                        "    PRIMARY KEY (id )\n" +
-                        ")";
+                String sql = "CREATE TABLE IF NOT EXISTS News " +
+                        "(id int(11) NOT NULL AUTO_INCREMENT, " +
+                        "Title TEXT, " +
+                        "Link TEXT, " +
+                        "Description TEXT null , " +
+                        "Author TEXT, " +
+                        "PublishedDate DATE, " +
+                        "RSSLink int(11) NOT NULL, " +
+                        "FOREIGN KEY (RSSLink) REFERENCES RSSChannel(id) ON UPDATE CASCADE ON DELETE RESTRICT, " +
+                        "PRIMARY KEY (id)" +
+                        ") ENGINE=InnoDB CHARSET=utf8mb4;";
                 stmt.executeUpdate(sql);
                 System.out.println("table News created.");
             } catch (SQLException e) {
+                e.printStackTrace();
                 System.out.println("no change is needed");
             }
         } catch (Exception se) {
             se.printStackTrace();
+        }
+    }
+
+    public static void RSSRead(String url) {
+        try {
+            System.out.println("Add data in RSSChannel table...");
+            SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL(url)));
+            try {
+                PreparedStatement pstm = conn.prepareStatement("INSERT INTO RSSChannel(RSSLink, Title, Link, Description, LastBuildDate) VALUES(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+                pstm.setString(1, url);
+                pstm.setString(2, feed.getTitle());
+                pstm.setString(3, feed.getLink());
+                pstm.setString(4, feed.getDescription());
+                pstm.setDate(5, new Date(feed.getPublishedDate().getTime()));
+                pstm.executeUpdate();
+                ResultSet rs = pstm.getGeneratedKeys();
+                if (rs.next()) {
+                    tableId = rs.getInt(1);
+                    System.out.println(tableId);
+                }
+                System.out.println("Add data in RSSChannel table finished.");
+            } catch (Exception ex) {
+                System.out.println("ERROR: " + ex.getMessage());
+            }
+
+            System.out.println("Add data in News table...");
+            for (SyndEntry syndEntry : feed.getEntries()) {
+                try {
+                    PreparedStatement pstm = conn.prepareStatement("INSERT INTO News(Title, Link, Description, Author, PublishedDate, RSSLink) VALUES(?, ?, ?, ?, ?, ?)");
+                    pstm.setString(1, syndEntry.getTitle());
+                    pstm.setString(2, syndEntry.getLink());
+                    if (syndEntry.getDescription() != null)
+                        pstm.setString(3, syndEntry.getDescription().getValue());
+                    else
+                        pstm.setString(3, null);
+                    pstm.setString(4, syndEntry.getAuthor());
+                    pstm.setDate(5, new Date(syndEntry.getPublishedDate().getTime()));
+                    pstm.setInt(6, tableId);
+                    pstm.executeUpdate();
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    System.out.println("ERROR: " + ex.getMessage());
+                }
+            }
+            System.out.println("Add data in RSSChannel table finished.");
+        } catch (Exception ex) {
+            ex.printStackTrace();
         } finally {
             try {
                 if (stmt != null)
@@ -122,5 +157,6 @@ public class DBOperations {
     public static void main(String[] args) {
         createDB();
         createTables();
+        RSSRead("http://www.iribnews.ir/fa/rss/allnews");
     }
 }
