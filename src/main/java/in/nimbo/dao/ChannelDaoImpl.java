f2bc3cc4ca7c36917d2ef46f2aefc4c90274f3d4
DanialErfanian <erfaniiyanerfan@gmail.com>
2019-07-08 13:21:13 +0430

    Use ConnectionPool Use DataSource and closing connections after use

--- src/main/java/in/nimbo/dao/ChannelDaoImpl.java
+++ src/main/java/in/nimbo/dao/ChannelDaoImpl.java
@@ -1,49 +1,60 @@
 package in.nimbo.dao;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import in.nimbo.entity.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
 
 public class ChannelDaoImpl implements ChannelDao {
-    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/";
-    private static final String DB_NAME = "RSSFeed";
-    //  Database credentials
-    private static final String USER = "root";
-    private static final String PASS = "";
-
-    private static Connection conn;
-    private static Statement stmt;
 
     private static final Logger logger = LoggerFactory.getLogger(ChannelDaoImpl.class);
+    private final DataSource source;
 
+    public ChannelDaoImpl(DataSource source) {
+        this.source = source;
+    }
 
-    static {
+    public ChannelDaoImpl() {
+        Properties props = new Properties();
+        FileInputStream fis;
+        MysqlDataSource mysqlDS = null;
         try {
-            Class.forName(JDBC_DRIVER);
-            conn = DriverManager.getConnection(DB_URL + DB_NAME + "?useSsl=false&useUnicode=yes&characterEncoding=UTF-8", USER, PASS);
-            stmt = conn.createStatement();
-            logger.info("connection reset");
-        } catch (SQLException | ClassNotFoundException e) {
-            logger.error("creating connection failed", e);
+            fis = new FileInputStream(new File("src/main/resources/db.properties"));
+            props.load(fis);
+            Class.forName(props.getProperty("MYSQL_DB_DRIVER_CLASS"));
+            mysqlDS = new MysqlDataSource();
+            mysqlDS.setURL(props.getProperty("MYSQL_DB_URL"));
+            mysqlDS.setUser(props.getProperty("MYSQL_DB_USERNAME"));
+            mysqlDS.setPassword(props.getProperty("MYSQL_DB_PASSWORD"));
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
         }
+        this.source = mysqlDS;
     }
 
     @Override
     public Channel getChannel(String url) {
+        Channel channel = null;
         try {
             PreparedStatement statement;
-            statement = conn.prepareStatement("SELECT * FROM RSSChannel WHERE RSSLink = ?");
+            Connection connection = source.getConnection();
+            statement = connection.prepareStatement("SELECT * FROM RSSChannel WHERE RSSLink = ?");
             statement.setString(1, url);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next())
-                return toChannel(resultSet);
+                channel = toChannel(resultSet);
+            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return null;
+        return channel;
     }
 
     private Channel toChannel(ResultSet resultSet) throws SQLException {
@@ -61,17 +72,20 @@ public class ChannelDaoImpl implements ChannelDao {
 
     @Override
     public Channel getChannel(int id) {
+        Channel channel = null;
         try {
             PreparedStatement statement;
-            statement = conn.prepareStatement("SELECT * FROM RSSChannel WHERE id = ?");
+            Connection connection = source.getConnection();
+            statement = connection.prepareStatement("SELECT * FROM RSSChannel WHERE id = ?");
             statement.setInt(1, id);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next())
-                return toChannel(resultSet);
+                channel = toChannel(resultSet);
+            connection.close();
         } catch (SQLException e) {
             logger.error("Error in converting resultSet to Channel Object ", e);
         }
-        return null;
+        return channel;
     }
 
     @Override
@@ -79,36 +93,42 @@ public class ChannelDaoImpl implements ChannelDao {
         String sql = "UPDATE RSSChannel" +
                 " SET RSSLink = ?, Title = ?, Link = ?, Description = ?, LastBuildDate = ?" +
                 " WHERE id = ?";
+        boolean res = false;
         try {
-            PreparedStatement statement = conn.prepareStatement(sql);
+            Connection connection = source.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
             statement.setInt(6, channel.getId());
             statement.setString(1, channel.getRSSUrl());
             statement.setString(2, channel.getTitle());
             statement.setString(3, channel.getLink());
             statement.setString(4, channel.getDescription());
             statement.setDate(5, new Date(channel.getLastUpdate().getTime()));
-            return statement.execute();
+            res = statement.execute();
+            connection.close();
         } catch (SQLException e) {
             logger.error("Error in update a row of RSSChannel table", e);
         }
-        return false;
+        return res;
     }
 
     @Override
     public boolean add(Channel channel) {
         String sql = "INSERT into RSSChannel(id, RSSLink, Title, Link, Description, LastBuildDate) VALUES(?,?,?,?,?,?)";
+        boolean res = false;
         try {
-            PreparedStatement statement = conn.prepareStatement(sql);
+            Connection connection = source.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
             statement.setInt(1, channel.getId());
             statement.setString(2, channel.getRSSUrl());
             statement.setString(3, channel.getTitle());
             statement.setString(4, channel.getLink());
             statement.setString(5, channel.getDescription());
             statement.setDate(6, new Date(channel.getLastUpdate().getTime()));
-            return statement.execute();
+            res = statement.execute();
+            connection.close();
         } catch (SQLException e) {
             logger.error("Error in insert into RSSChannel table", e);
         }
-        return false;
+        return res;
     }
 }
